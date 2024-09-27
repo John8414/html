1aefe171484fd8c04f502fad805a17cc24005978
Nguyen.Huynh <jmnguyen276@gmail.com>
2024-09-28 04:23:13 +0700

    change to plain html

---
+++ app.js
@@ -0,0 +1,17 @@
+const imageContainer = document.querySelector('.img-scale');
+const img = imageContainer.querySelector('img');
+
+imageContainer.addEventListener('mousemove', (e) => {
+  const rect = imageContainer.getBoundingClientRect();
+  const x = e.clientX - rect.left; 
+  const y = e.clientY - rect.top; 
+  
+  const xPercent = (x / rect.width) * 100;
+  const yPercent = (y / rect.height) * 100;
+
+  img.style.transformOrigin = `${xPercent}% ${yPercent}%`;
+});
+
+imageContainer.addEventListener('mouseleave', () => {
+  img.style.transformOrigin = 'center center'; 
+});

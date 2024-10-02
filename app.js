5161a01db27ab187e74f8b66770cd2996c0527f4
Nguyen.Huynh <jmnguyen276@gmail.com>
2024-10-03 01:31:09 +0700

    add js and style

--- app.js
+++ app.js
@@ -1,22 +1,77 @@
-const imageContainer = document.querySelector('.img-scale');
-const img = imageContainer.querySelector('img');
+// Scale zoom in image //
+const imageContainers = document.querySelectorAll('.img-scale');
 
-imageContainer.addEventListener('mousemove', (e) => {
-  const rect = imageContainer.getBoundingClientRect();
-  const x = e.clientX - rect.left; 
-  const y = e.clientY - rect.top; 
+imageContainers.forEach((imageContainer) => {
+  const img = imageContainer.querySelector('img');
   
-  const xPercent = (x / rect.width) * 100;
-  const yPercent = (y / rect.height) * 100;
-
-  img.style.transformOrigin = `${xPercent}% ${yPercent}%`;
+  const handleMouseMove = (e) => {
+    const rect = imageContainer.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    
+    const xPercent = (x / rect.width) * 100;
+    const yPercent = (y / rect.height) * 100;
+    
+    img.style.transformOrigin = `${xPercent}% ${yPercent}%`;
+  };
+  
+  const handleMouseLeave = () => {
+    img.style.transformOrigin = 'center center';
+  };
+  
+  imageContainer.addEventListener('mousemove', handleMouseMove);
+  imageContainer.addEventListener('mouseleave', handleMouseLeave);
+  
+  const removeZoomEffect = () => {
+    imageContainer.removeEventListener('mousemove', handleMouseMove);
+    imageContainer.removeEventListener('mouseleave', handleMouseLeave);
+  };
+  
 });
-
-imageContainer.addEventListener('mouseleave', () => {
-  img.style.transformOrigin = 'center center'; 
-});
+// Scale zoom in image //
 
 
+// add search input //
 document.querySelector('.search-text').addEventListener('click', function() {
   document.querySelector('.search-header').classList.add('active');
+});
+
+// slick //
+$(document).ready(function(){
+  $('.product-slider').slick({
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: $('.prev-btn'), // Custom previous button
+    nextArrow: $('.next-btn'), // Custom next button
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
 });
